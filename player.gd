55b4978f12fdffd06637dd97739472b4025e981e
Borrowed Time <bt@aero.cx>
2023-12-30 01:51:04 -0800

    player rotates in moving direction

--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ func _physics_process(delta):
 	
 	#Movement (600 Pixels * Tick)
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	rotation = direction.angle()
 	velocity = direction * 600
 	move_and_slide()
 	
